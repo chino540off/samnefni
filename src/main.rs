6b2491510f13041e376bff4471c6216e15436e5d
Olivier Détour <odetour@antidot.net>
2024-08-01 15:18:08 +0200

    [src] bootstrap first command line

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 extern crate serde;
 
+mod cli;
 mod config;
 mod model;
 
+use cli::clap::CommandFactory;
+use cli::clap::Parser;
+
 fn main() {
     let conf = config::Config::new();
-    println!("{:?}", conf);
+    let cli = cli::Parser::parse();
+
+    match &cli.command {
+        cli::Command::Exec { command } => {
+            println!("{:?}", conf);
+            println!("'exec' was used, command is: {command:?}");
+        }
+        cli::Command::Completion { shell } => {
+            let mut cmd = cli::Parser::command();
+            cli::print_completions(shell.clone(), &mut cmd);
+        }
+    }
 }
