ec2a6add3ba0d17d8f5cc8d25b07dea197e59419
Olivier Détour <odetour@antidot.net>
2024-10-08 18:03:43 +0200

    Migrate tests

--- src/config/tests.rs
+++ src/config/tests.rs
@@ -22,50 +22,71 @@ fn path_set() {
     std::env::remove_var("SAMNEFNI_CONFIG");
 }
 
-#[rstest]
-fn simple() {
-    let conf: Config = toml::from_str(
+#[fixture]
+fn conf() -> Config {
+    toml::from_str(
         r#"
             [aliases.cmd1]
-                a = { args = ["add_c1"] }
-                b = { args = ["bing_c1", "--opt-c1", "value"] }
+                a = "add_c1"
+                b = "bing_c1 --opt-c1 value"
             [aliases.cmd2]
-                a = { args = ["add_c2"] }
-                b = { args = ["bing_c2", "--opt-c2", "value"] }
+                a = "add_c2"
+                b = "bing_c2 --opt-c2 value"
+            [aliases.cmd3]
+                a = "add_c3 ..."
+                b = "bing_c3 --opt-c3..."
+                c = "call_c3 --opt-c3... --last"
         "#,
     )
-    .unwrap();
+    .unwrap()
+}
 
+#[rstest]
+fn simple(conf: Config) {
     assert_eq!(
         conf.aliases.get("cmd1").unwrap().get("a").unwrap(),
-        &model::Alias::Simple {
-            args: vec!["add_c1".to_string()]
-        }
+        &model::Arguments(vec![model::Argument::Simple("add_c1".to_string())]),
     );
     assert_eq!(
         conf.aliases.get("cmd1").unwrap().get("b").unwrap(),
-        &model::Alias::Simple {
-            args: vec![
-                "bing_c1".to_string(),
-                "--opt-c1".to_string(),
-                "value".to_string()
-            ]
-        }
+        &model::Arguments(vec![
+            model::Argument::Simple("bing_c1".to_string()),
+            model::Argument::Simple("--opt-c1".to_string()),
+            model::Argument::Simple("value".to_string()),
+        ]),
     );
     assert_eq!(
         conf.aliases.get("cmd2").unwrap().get("a").unwrap(),
-        &model::Alias::Simple {
-            args: vec!["add_c2".to_string()]
-        }
+        &model::Arguments(vec![model::Argument::Simple("add_c2".to_string()),]),
     );
     assert_eq!(
         conf.aliases.get("cmd2").unwrap().get("b").unwrap(),
-        &model::Alias::Simple {
-            args: vec![
-                "bing_c2".to_string(),
-                "--opt-c2".to_string(),
-                "value".to_string()
-            ]
-        }
+        &model::Arguments(vec![
+            model::Argument::Simple("bing_c2".to_string()),
+            model::Argument::Simple("--opt-c2".to_string()),
+            model::Argument::Simple("value".to_string()),
+        ]),
+    );
+    assert_eq!(
+        conf.aliases.get("cmd3").unwrap().get("a").unwrap(),
+        &model::Arguments(vec![
+            model::Argument::Simple("add_c3".to_string()),
+            model::Argument::Fold("".to_string()),
+        ]),
+    );
+    assert_eq!(
+        conf.aliases.get("cmd3").unwrap().get("b").unwrap(),
+        &model::Arguments(vec![
+            model::Argument::Simple("bing_c3".to_string()),
+            model::Argument::Fold("--opt-c3".to_string()),
+        ]),
+    );
+    assert_eq!(
+        conf.aliases.get("cmd3").unwrap().get("c").unwrap(),
+        &model::Arguments(vec![
+            model::Argument::Simple("call_c3".to_string()),
+            model::Argument::Fold("--opt-c3".to_string()),
+            model::Argument::Simple("--last".to_string()),
+        ]),
     );
 }
