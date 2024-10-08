ab423d8ef7866a0ac7d1bc143a3f039c53d193d7
Olivier Détour <odetour@antidot.net>
2024-10-08 18:00:49 +0200

    Update model to support fold arguments

--- src/model.rs
+++ src/model.rs
@@ -1,13 +1,37 @@
-use serde::Deserialize;
-
 pub type Program = String;
-pub type Argument = String;
-pub type Arguments = Vec<Argument>;
 
-#[derive(Deserialize, Debug, PartialEq)]
-#[serde(untagged)]
-pub enum Alias {
-    Simple { args: Arguments },
+#[derive(Debug, PartialEq, Clone)]
+pub enum Argument {
+    Simple(String),
+    Fold(String),
 }
 
-pub type Aliases = std::collections::HashMap<String, Alias>;
+impl From<String> for Argument {
+    fn from(s: String) -> Self {
+        match s.find("...") {
+            Some(i) => Argument::Fold(s.split_at(i).0.to_string()),
+            None => Argument::Simple(s),
+        }
+    }
+}
+
+impl From<&str> for Argument {
+    fn from(s: &str) -> Self {
+        From::from(s.to_string())
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Arguments(pub Vec<Argument>);
+
+impl<'de> serde::Deserialize<'de> for Arguments {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Ok(Arguments(s.split(' ').map(From::from).collect()))
+    }
+}
+
+pub type Aliases = std::collections::HashMap<String, Arguments>;
